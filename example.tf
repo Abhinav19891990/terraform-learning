d42584b264ed3ac31a698e76ea56b4f02ac9f759
Abhinav19891990 <abhinav.mudunuri1991@gmail.com>
2021-01-13 15:46:56 -0800

    Added AWS credentials

--- example.tf
+++ example.tf
@@ -2,7 +2,10 @@ provider "aws" {
   profile = "default"
   //region  = "us-west-2"
   region = var.region
+  access_key = "AKIAXHAWTAVMRK5W4OPK"
+  secret_key = "uu3l7wPhSP4slLfpKBQRsa8PyHgO5kIiG1oDgny1"
 }
+
 
 resource "aws_instance" "example" {
   ami           = "ami-830c94e3"
