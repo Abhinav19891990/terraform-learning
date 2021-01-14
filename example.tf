2cff7e430d137bf252b09cc9fb34bd30aa0fcb30
Abhinav19891990 <abhinav.mudunuri1991@gmail.com>
2021-01-13 18:08:35 -0800

    added backend

--- example.tf
+++ example.tf
@@ -1,7 +1,40 @@
+terraform{
+  backend "remote" {
+  organization = "terraform-abhinav343"
+  workspaces {
+      name = "terraform-learning"
+    }
+  }
+
 provider "aws" {
   profile = "default"
   //region  = "us-west-2"
-  region = var.region
+  region = var.regioncd ../
+}
+
+
+resource "aws_instance" "example" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+}
+
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.example.id
+}
+
+}
+backend "remote" {
+  organization = "<ORG_NAME>"
+  workspaces {
+      name = "Example-Workspace"
+    }
+  }
+
+provider "aws" {
+  profile = "default"
+  //region  = "us-west-2"
+  region = var.regioncd ../
 }
 
 
