b5462bd2eb2036e614551b28034ac081b85b8c93
Abhinav19891990 <abhinav.mudunuri1991@gmail.com>
2021-01-13 18:31:26 -0800

    Changes

--- example.tf
+++ example.tf
@@ -1,17 +1,16 @@
 terraform{
-  backend "remote" {
+backend "remote" {
   organization = "terraform-abhinav343"
   workspaces {
       name = "terraform-learning"
     }
-  }
+}
 
 provider "aws" {
   profile = "default"
   //region  = "us-west-2"
-  region = var.regioncd ../
+  region = var.region
 }
-
 
 resource "aws_instance" "example" {
   ami           = "ami-830c94e3"
@@ -22,28 +21,4 @@ resource "aws_eip" "ip" {
   vpc      = true
   instance = aws_instance.example.id
 }
-
-}
-backend "remote" {
-  organization = "<ORG_NAME>"
-  workspaces {
-      name = "Example-Workspace"
-    }
-  }
-
-provider "aws" {
-  profile = "default"
-  //region  = "us-west-2"
-  region = var.regioncd ../
-}
-
-
-resource "aws_instance" "example" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-}
-
-resource "aws_eip" "ip" {
-  vpc      = true
-  instance = aws_instance.example.id
 }
